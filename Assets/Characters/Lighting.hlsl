b3df667e708a3e52aa071251f5294f839c58009f
Zanejjb <mavicreates@gmail.com>
2024-11-09 04:44:05 -0800

    worked on the hub area :>

--- Assets/Characters/Lighting.hlsl
+++ Assets/Characters/Lighting.hlsl
@@ -1,6 +1,7 @@
 void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color,
 	out float DistanceAtten, out float ShadowAtten)
 {
+
 #ifdef SHADERGRAPH_PREVIEW
 	Direction = normalize(float3( 0.5f, 0.5f, 0.25f));
 	Color = float3(1.0f, 1.0f, 1.0f);
